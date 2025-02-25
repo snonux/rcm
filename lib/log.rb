577d457b1bd0f09a91be2afa07a9f81c38cac8a2
Paul Buetow <paul@buetow.org>
2025-02-25 22:50:21 +0200

    change logger

--- lib/log.rb
+++ lib/log.rb
@@ -3,6 +3,9 @@ require 'logger'
 module RCM
   module Log
     @@logger = Logger.new(STDOUT)
+    @@logger.formatter = proc do |severity, datetime, progname, msg|
+      "#{severity} #{Time.now.strftime('%Y%m%d-%H%M%S')} #{msg}\n"
+    end
 
     def info(message) = @@logger.info("#{id} => #{message}")
     def warn(message) = @@logger.warn("#{id} => #{message}")
